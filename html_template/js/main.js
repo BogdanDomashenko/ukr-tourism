4eb886bd04194b9056af83caf0a5193ad84ef53e
BogdanDomashenko <got.bogdan@gmail.com>
2021-10-18 23:26:29 +0300

    update

--- html_template/js/main.js
+++ html_template/js/main.js
@@ -32,14 +32,55 @@ for (let anchor of anchors) {
   });
 
 
+// menu
+
+
+// window.addEventListener("scroll", function() {
+//   console.log(navbar);
+//   navbar.classList.toggle("sticky", window.scrollY > 0);
+// });
+
+// mobile menu
+let navBarMobile = new Vue({
+  el: '#navbar',
+  data: {
+    isActive: false,
+    isScrolling: false,
+    error: null
+  },
+  beforeMount() {
+    window.addEventListener("scroll", this.handleScroll);
+},
+beforeDestroy() {
+  window.addEventListener("scroll", this.handleScroll);
+},
+methods: {
+  greet: function(event) {
+    this.isActive = !this.isActive;
+  },
+  handleScroll: function(event) {
+    if (window.scrollY > 50) {
+      if(!this.isScrolling) {
+        this.isScrolling = true;
+      }
+    }
+    else {
+      this.isScrolling = false;
+    }
+  }
+},
+});
+
+
+
   // transport-chart
-  const transportChart1 = document.querySelector('#transport-chart1');
-  const transportChart2 = document.querySelector('#transport-chart2');
+//   const transportChart1 = document.querySelector('#transport-chart1');
+//   const transportChart2 = document.querySelector('#transport-chart2');
 
-createTransportChart(transportChart1, 155, 124);
-createTransportChart(transportChart2, 155, 155);
+// createTransportChart(transportChart1, 155, 155);
+// createTransportChart(transportChart2, 155, 124);
 
-function createTransportChart(element, maxELCount, elCount) {
-  element.style.width = ((elCount/maxELCount)*100) + "%";
-  element.style.height = element.clientWidth + "px";
-}
+// function createTransportChart(element, maxELCount, elCount) {
+//   element.style.width = ((elCount/maxELCount)*100) + "%";
+//   element.style.height = element.clientWidth + "px";
+// }
